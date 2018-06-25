344956d46051b4ceb0b64773974355c4a736ef95
Daniel <dan@dragomirweb.com>
2018-06-25 23:49:45 +0300

    add header component to app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <div id="app">
+  <section id="app">
+    <Header></Header>
     <img src="./assets/logo.png">
     <router-view></router-view>
-  </div>
+  </section>
 </template>
 
 <script>
+import Header from "@/components/Header";
+
 export default {
-  name: 'app'
-}
+  name: "app",
+  components: {
+    "Header" : Header
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
