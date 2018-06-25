364c7e8828d61068b4f67ca803274ae755e6e43c
Daniel <dan@dragomirweb.com>
2018-06-25 22:04:37 +0300

    git init vue-bp

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <b-navbar type="light" variant="light">
+    <b-nav-form>
+      <b-input-group left="@">
+        <b-form-input class="mr-sm-2" type="text" placeholder="Username"></b-form-input>
+      </b-input-group>
+    </b-nav-form>
+  </b-navbar>
+</div>
+</template>
+<script>
+
+
+export default {
+  name: "header",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss">
+</style>
