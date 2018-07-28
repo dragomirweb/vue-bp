912c6cae2cf107b4c0b90a3e63fae96d3e034a6c
Daniel <dan@dragomirweb.com>
2018-07-28 19:26:46 +0300

    add include of vue moment in vue instance

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/scss/index.scss'
 
-
 Vue.use(BootstrapVue)
+Vue.use(require('vue-moment'))
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
