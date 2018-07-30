07a5d042a9ff93ba51d4f3572e68a2a2dd746dda
Dragomir Dan <dan@1stweb.dk>
2018-07-30 13:28:18 +0300

    add new route for matches components

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/components/Home";
-
+import Matches from "@/components/Matches";
 Vue.use(Router);
 
 export default new Router({
@@ -15,6 +15,11 @@ export default new Router({
       path: "/",
       name: "Home",
       component: Home
+    },
+    {
+      path: "/matches",
+      name: "Matches",
+      component: Matches
     }
   ]
 });
