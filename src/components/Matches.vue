a5c16fd823a6f37fa18dc64aadc683cb8852bb85
Dragomir Dan <dan@1stweb.dk>
2018-07-30 13:26:42 +0300

    add matches components

---
+++ src/components/Matches.vue
@@ -0,0 +1,23 @@
+<template>
+<section>
+    <h1>hello matches component</h1>
+</section>
+</template>
+
+<script>
+export default {
+    name: "Matches",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
